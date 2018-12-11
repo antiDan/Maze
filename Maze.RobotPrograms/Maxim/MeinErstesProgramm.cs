03306702bd4fcca4e8e5bd9c158af4525575fb2f
dan <antipindaniil@mail.ru>
2018-12-11 21:30:27 +0100

    Das erste Programm vom Maxim

---
+++ Maze.RobotPrograms/Maxim/MeinErstesProgramm.cs
@@ -0,0 +1,25 @@
+﻿using Maze.Core.Interfaces;
+
+namespace Maze.RobotPrograms.Maxim
+{
+    public class MeinErstesProgramm : IInfoLevelProgram
+    {
+        public void Program(ILevelInfo robot)
+        {
+            var leftIsWall = robot.IsLeftWall();
+
+            if (leftIsWall)
+            {
+                robot.GoRight();
+                robot.GoRight();
+                robot.GoRight();
+            }
+            else
+            {
+                robot.GoLeft();
+                robot.GoLeft();
+                robot.GoLeft();
+            }
+        }
+    }
+}
