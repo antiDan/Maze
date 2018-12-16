e1858c2dded32f33b91a95a434d5ba509e6a52f5
dan <antipindaniil@mail.ru>
2018-12-16 02:10:52 +0100

    Das erste universale Programm

---
+++ Maze.RobotPrograms/Test/UniProgram.cs
@@ -0,0 +1,151 @@
+﻿using Maze.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Maze.RobotPrograms.Test
+{
+    public class UniProgram : IInfoLevelProgram
+    {
+        public void Program(ILevelInfo robot)
+        {
+            var size = 16;
+            var startPoint = size / 2;
+
+            var cells = new Cell[size, size];
+            var stack = new Stack<Cell>();
+
+            var cell = new Cell(robot, cells, 0, 0);
+            stack.Push(cell);
+
+            while (cell.NewMoves > 0 || stack.Count > 0)
+            {
+                if (cell.NewMoves > 0)
+                {
+                    cell = cell.MoveInNewCell();
+                    stack.Push(cell);
+                }
+                else
+                {                    
+                    this.GoBack(stack.Pop(), stack.Peek(), robot);
+                    cell = stack.Peek();
+                }
+            }
+        }
+
+        private void GoBack(Cell current, Cell previous, ILevelInfo robot)
+        {
+            if (previous.X < current.X)
+            {
+                robot.GoLeft();
+            }
+            else if (previous.Y < current.Y)
+            {
+                robot.GoUp();
+            }
+            else if (previous.X > current.X)
+            {
+                robot.GoRight();
+            }
+            else if (previous.Y > current.Y)
+            {
+                robot.GoDown();
+            }
+            else
+            {
+                throw new Exception("Ich kann nicht zurück");
+            }
+        }
+    }
+
+    public class Cell
+    {
+        private ILevelInfo robot;
+        private Cell[,] cells;
+
+        public Cell(ILevelInfo robot, Cell[,] cells, int x, int y)
+        {
+            this.robot = robot;
+            this.cells = cells;
+            this.X = x;
+            this.Y = y;
+
+            this.cells[this.X, this.Y] = this;
+            this.CheckWalls();
+        }
+
+        public int X { get; }
+        public int Y { get; }
+
+        public bool? IsLeftWall { get; set; }
+        public bool? IsUpWall { get; set; }
+        public bool? IsRightWall { get; set; }
+        public bool? IsDownWall { get; set; }
+
+        public Cell Left => this.cells[this.X - 1, this.Y];
+        public Cell Top => this.cells[this.X, this.Y - 1];
+        public Cell Right => this.cells[this.X + 1, this.Y];
+        public Cell Bottom => this.cells[this.X, this.Y + 1];
+
+        public int NewMoves
+        {
+            get
+            {
+                var count = 0;
+
+                if (this.IsLeftNewMove) count++;
+                if (this.IsTopNewMove) count++;
+                if (this.IsRightNewMove) count++;
+                if (this.IsBottomNewMove) count++;
+
+                return count;
+            }
+        }
+
+        public bool IsLeftNewMove => this.IsLeftWall == false && this.Left == null;
+        public bool IsTopNewMove => this.IsUpWall == false && this.Top == null;
+        public bool IsRightNewMove => this.IsRightWall == false && this.Right == null;
+        public bool IsBottomNewMove => this.IsDownWall == false && this.Bottom == null;
+
+        public void CheckWalls()
+        {
+            this.IsLeftWall = robot.IsLeftWall();
+            this.IsUpWall = robot.IsUpWall();
+            this.IsRightWall = robot.IsRightWall();
+            this.IsDownWall = robot.IsDownWall();
+        }
+
+        public Cell MoveInNewCell()
+        {
+            if (this.IsLeftNewMove)
+            {
+                this.robot.GoLeft();
+                return new Cell(this.robot, this.cells, this.X - 1, this.Y);
+            }
+
+            if (this.IsTopNewMove)
+            {
+                this.robot.GoUp();
+                return new Cell(this.robot, this.cells, this.X, this.Y - 1);
+            }
+
+            if (this.IsRightNewMove)
+            {
+                this.robot.GoRight();
+                return new Cell(this.robot, this.cells, this.X + 1, this.Y);
+            }
+
+            if (this.IsBottomNewMove)
+            {
+                this.robot.GoDown();
+                return new Cell(this.robot, this.cells, this.X, this.Y + 1);
+            }
+
+            throw new Exception("Es gibt keine neue Zelle");
+        }
+
+        public override string ToString()
+        {
+            return $"{this.X}/{this.Y}";
+        }
+    }
+}
